2de8375484541700d51d522f2ca7e1c107c229cd
jaymwin <jmwiniarski@gmail.com>
2022-05-25 14:46:26 -0500

    doesn't work now for 2021 either

--- generate_annual_report.R
+++ generate_annual_report.R
@@ -15,7 +15,7 @@ source(here::here('scripts/waterfowl_survey_functions.R')) # functions for summa
 # what is the current survey year?
 # need minus 1 for now to replicate 2021 report
 analysis_year <- lubridate::today() %>% 
-  lubridate::year() # - 1 # -1 just needed for this example 2021
+  lubridate::year() - 1 # -1 just needed for this example 2021
 
 # append new data to database, summarize count data, run state-space analyses
 analyze_survey_data(analysis_year)
