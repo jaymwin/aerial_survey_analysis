cdfc55826ae3c0e1e301056f540620dd147285b0
jaymwin <jmwiniarski@gmail.com>
2023-06-01 15:15:49 -0500

    adding code to incorporate/combine 2023 data

--- scripts/01-import_new_survey_data.R
+++ scripts/01-import_new_survey_data.R
@@ -10,12 +10,15 @@ library(dbplyr)
 library(RSQLite)
 library(haven)
 library(janitor)
+library(readxl)
 
 source(here::here('scripts/waterfowl_survey_functions.R')) # functions for summarizing, analyzing data
 
 
 # import data from sas (2022) --------------------------------------
 
+# need an extra step here as data is originally in SAS format;
+# needs to be YYYY_air_wetlands.csv or YYYY_waterfowl.csv
 if (analysis_year == 2022) {
 
 sas_files <- 
@@ -58,6 +61,60 @@ sas_files %>%
   ) %>%
   write_csv(str_c(here::here('raw_data/annual_survey_data'), '/', analysis_year, '_air_wetlands.csv'))
 
+}
+
+# 2023 also slightly different; 3 separate spreadsheets (air counts, ground counts, unoccupied air wetlands)
+# also in excel format
+if (analysis_year == 2023) {
+ 
+  excel_files <- 
+    dir_ls(here::here('raw_data/annual_survey_data'), glob = '*xlsx') %>%
+    as_tibble() %>%
+    mutate(
+      type = str_extract(value, 'air_wetlands|waterfowl|ground'),
+      name = str_remove(basename(value), '.excel'),
+      year = str_sub(name, start = 1, end = 4)
+    )
+  
+  # wetlands
+  excel_files %>%
+    filter(type == 'air_wetlands' & year == analysis_year) %>%
+    pull(value) %>%
+    read_excel() %>%
+    clean_names() %>%
+    # get rid of any NA rows
+    filter(!is.na(month)) %>%
+    mutate(
+      ground = replace_na(ground, 0),
+      region = case_when(
+        transect < 30 ~ 1, # SEC
+        transect >= 30 & transect < 43 ~ 2, # NHI
+        transect >= 43 & transect < 61 ~ 3, # NLO
+        transect >= 61 & transect < 72 ~ 4 # SWD
+      )
+    ) %>%
+    # there are some NAs, remove them
+    mutate_at(vars(typ1:dit), ~replace(., is.na(.), 0)) %>%
+    write_csv(str_c(here::here('raw_data/annual_survey_data'), '/', analysis_year, '_air_wetlands.csv'))
+  
+  # air waterfowl counts
+  air_waterfowl <-
+    excel_files %>%
+    filter(type == 'waterfowl' & year == analysis_year) %>%
+    pull(value) %>%
+    read_excel() %>%
+    clean_names() %>%
+    mutate(across(-c(direct, side), as.numeric))
+  
+  # air waterfowl counts
+  ground_waterfowl <-
+    excel_files %>%
+    filter(type == 'ground' & year == analysis_year) %>%
+    pull(value) %>%
+    read_excel() %>%
+    clean_names() %>%
+    mutate(across(-c(direct, side), as.numeric))
+   
 }
 
 
